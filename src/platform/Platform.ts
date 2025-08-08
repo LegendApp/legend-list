2026de354744dcbfa43bc5308eb943ab0a795a09
Jay Meistrich <jmeistrich@gmail.com>
2025-08-08 14:08:59 +0100

    Fix a web type

--- src/platform/Platform.ts
+++ src/platform/Platform.ts
@@ -1,7 +1,8 @@
 export const Platform = {
     isTesting: false,
     isTV: false,
-    OS: "web" as "web" | "ios" | "android",
+    // Widen the type to avoid unreachable-branch lints in cross-platform code that compares against other OSes
+    OS: "web" as "web" | "ios" | "android" | string,
     select: <T>(specifics: { web?: T; default?: T } & { [platform: string]: T }): T => {
         return specifics.web ?? specifics.default!;
     },
