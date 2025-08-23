aeb35d473315c028563ea01ebdad2dc244abf66d
Jay Meistrich <jmeistrich@gmail.com>
2025-08-24 00:07:21 +0100

    better style for initial scroll index example

--- example-web/src/examples/InitialScrollIndexExample.tsx
+++ example-web/src/examples/InitialScrollIndexExample.tsx
@@ -8,25 +8,24 @@ type Row = { id: string; type: "item" | "separator" };
 
 export default function InitialScrollIndexExample() {
     const data = React.useMemo<Row[]>(
-        () => Array.from({ length: 500 }, (_, i) => ({ id: String(i), type: "item" })),
+        () => Array.from({ length: 500 }, (_, i) => ({ id: String(i), type: i % 3 === 0 ? "separator" : "item" })),
         [],
     );
     return (
         <View style={{ background: "#456", display: "flex", flex: 1, minHeight: 0, position: "relative" }}>
             <LegendList<Row>
                 data={data}
-                drawDistance={1000}
+                drawDistance={2000}
                 estimatedItemSize={200}
                 // getEstimatedItemSize={(i) => (data[i].type === "separator" ? 52 : 400)}
-                initialScrollIndex={300}
+                initialScrollIndex={200}
                 keyExtractor={(it) => it?.id}
-                maintainVisibleContentPosition
                 renderItem={({ item, index }: { item: Row; index: number }) =>
                     item.type === "separator" ? (
                         <View
                             style={{
                                 alignItems: "center",
-                                backgroundColor: "red",
+                                backgroundColor: "black",
                                 height: 52,
                                 justifyContent: "center",
                             }}
@@ -37,8 +36,8 @@ export default function InitialScrollIndexExample() {
                         <View
                             style={{
                                 alignItems: "center",
-                                background: index % 2 ? "#f0f0f0" : "#eaeaea",
-                                height: 400,
+                                background: index % 2 ? "#f0f0f0" : "#ccc",
+                                height: 200 + Math.random() * 200,
                                 justifyContent: "center",
                             }}
                         >
