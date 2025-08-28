e945ec468984eacb4b8ff6957ef8295143304280
Jay Meistrich <jmeistrich@gmail.com>
2025-08-28 21:51:43 +0100

    fix up types of useSyncLayout

--- src/components/LeanLayoutView.tsx
+++ src/components/LeanLayoutView.tsx
@@ -1,4 +1,10 @@
 import { useSyncLayout } from "@/hooks/useSyncLayout";
+import type { LayoutRectangle } from "@/platform/Layout.native";
+
+interface LeanLayoutViewProps {
+    refView: React.RefObject<HTMLDivElement>;
+    onLayoutChange: (rectangle: LayoutRectangle, fromLayoutEffect: boolean) => void;
+}
 
 export const LeanLayoutView = ({ onLayoutChange, refView, ...rest }: LeanLayoutViewProps) => {
     useSyncLayout({ onLayoutChange, ref: refView });
