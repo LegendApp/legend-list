4c5e18a1ae7a0ef708169f8af8a6f2527acf7527
Jay Meistrich <jmeistrich@gmail.com>
2025-08-28 19:16:31 +0100

    split some function by platform

--- src/constants.ts
+++ src/constants.ts
@@ -6,6 +6,3 @@ export const POSITION_OUT_OF_VIEW = -10000000;
 // blue arrow at the rights means negative scrollAdjust (used for maintainVisibleContentPosition) negative values
 export const ENABLE_DEVMODE = __DEV__ && false;
 export const ENABLE_DEBUG_VIEW = __DEV__ && false;
-
-// @ts-expect-error nativeFabricUIManager is not defined in the global object types
-export const IsNewArchitecture = global.nativeFabricUIManager != null;
