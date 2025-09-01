df1e33e0ec9968a2efb5eccb254d6f01ebcef110
Jay Meistrich <jmeistrich@gmail.com>
2025-09-01 11:14:17 +0200

    fix build issues for web

--- src/components/Container.tsx
+++ src/components/Container.tsx
@@ -1,4 +1,5 @@
-import type * as React from "react";
+// biome-ignore lint/style/useImportType: Leaving this out makes it crash in some environments
+import * as React from "react";
 import { useEffect, useMemo, useRef, useState } from "react";
 import type { View } from "react-native";
 
