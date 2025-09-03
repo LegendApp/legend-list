278bdfd3a286ad726c4492b3a04f738ed9f22a0b
Jay Meistrich <jmeistrich@gmail.com>
2025-09-03 09:34:40 +0200

    fix web was not flattening styles

--- src/platform/StyleSheet.tsx
+++ src/platform/StyleSheet.tsx
@@ -1,4 +1,10 @@
+function flattenStyles<T>(styles: T | T[]): T {
+    if (Array.isArray(styles)) {
+        return Object.assign({}, ...styles.filter(Boolean));
+    }
+    return styles;
+}
 export const StyleSheet = {
     create: <T extends Record<string, any>>(styles: T): T => styles,
-    flatten: (style: any): any => style,
+    flatten: (style: any): any => flattenStyles(style),
 };
