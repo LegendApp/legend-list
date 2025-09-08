7053b087b4ac2b02a5c34654b6f38c82e76fb71b
Jay Meistrich <jmeistrich@gmail.com>
2025-09-08 14:40:44 +0200

    feat: set activeStickyIndex for usage in getState()

--- src/types.ts
+++ src/types.ts
@@ -462,6 +462,7 @@ export interface LegendListRenderItemProps<
 }
 
 export type ScrollState = {
+    activeStickyIndex: number | undefined;
     contentLength: number;
     data: readonly any[];
     end: number;
