6c8ae4c8b5537d4c69d06a78ab9c415576f1fd58
Jay Meistrich <github@jayz.us>
2025-09-14 14:32:53 +0200

    fix: change Separator to use useIsLastItem which should reduce the number of times it runs

--- src/components/Separator.tsx
+++ src/components/Separator.tsx
@@ -1,7 +1,7 @@
 // biome-ignore lint/style/useImportType: Leaving this out makes it crash in some environments
 import * as React from "react";
 
-import { useArr$ } from "@/state/state";
+import { useIsLastItem } from "@/state/ContextContainer";
 
 export interface SeparatorProps<ItemT> {
     ItemSeparatorComponent: React.ComponentType<{ leadingItem: ItemT }>;
@@ -10,8 +10,7 @@ export interface SeparatorProps<ItemT> {
 }
 
 export function Separator<ItemT>({ ItemSeparatorComponent, itemKey, leadingItem }: SeparatorProps<ItemT>) {
-    const [lastItemKeys] = useArr$(["lastItemKeys"]);
-    const isALastItem = lastItemKeys.includes(itemKey);
+    const isLastItem = useIsLastItem();
 
-    return isALastItem ? null : <ItemSeparatorComponent leadingItem={leadingItem} />;
+    return isLastItem ? null : <ItemSeparatorComponent leadingItem={leadingItem} />;
 }
