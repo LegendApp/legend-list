49c7993cc9de66d1b8133ee5486a4d03f3c60919
Jay Meistrich <github@jayz.us>
2025-09-15 10:45:11 +0200

    fix: crash when using snapTo in some environments

--- src/components/SnapWrapper.tsx
+++ src/components/SnapWrapper.tsx
@@ -1,3 +1,5 @@
+// biome-ignore lint/style/useImportType: Leaving this out makes it crash in some environments
+import React from "react";
 import type { ScrollView, ScrollViewProps } from "react-native";
 
 import { useArr$ } from "@/state/state";
