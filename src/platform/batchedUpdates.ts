9ffd84c5ebd92b53a02057aa38abc44b42a3e75e
Sophie L <beartechtalks@gmail.com>
2025-09-17 02:13:14 +0100

    fix:remove non-existent import on web
    the previous fix was still broken when using Vite, as it still tried to import the function. hopefully this shouldn't change anything functionality-wise

--- src/platform/batchedUpdates.ts
+++ src/platform/batchedUpdates.ts
@@ -1,5 +1,3 @@
-import { unstable_batchedUpdates } from "react-native";
-
-const batchedUpdates = unstable_batchedUpdates || ((callback: () => void) => callback());
+const batchedUpdates = ((callback: () => void) => callback());
 
 export { batchedUpdates };
