b9ad6a8481357ab0b5bdbd70567192e9ce6ecebc
Jay Meistrich <jmeistrich@gmail.com>
2025-10-06 13:38:43 +0200

    fix: a file was not importing React

--- src/components/LayoutView.tsx
+++ src/components/LayoutView.tsx
@@ -1,4 +1,5 @@
-import type * as React from "react";
+// biome-ignore lint/style/useImportType: Leaving this out makes it crash in some environments
+import * as React from "react";
 import { type RefObject, useRef } from "react";
 import { type LayoutRectangle, View, type ViewProps } from "react-native";
 
