bcb747c69a92033903d76a2cb61a7e0dcad4a8c4
Jay Meistrich <jmeistrich@gmail.com>
2025-10-16 15:46:59 +0200

    web bidrectional example has random heights to test that it actually works

--- example-web/src/examples/BidirectionalInfiniteListExample.tsx
+++ example-web/src/examples/BidirectionalInfiniteListExample.tsx
@@ -4,14 +4,39 @@ import { LegendList } from "@legendapp/list";
 import type { SimpleItem } from "./utils";
 import { generateItems } from "./utils";
 
+// A random number generator to simulate the size of the items
+// that we can seed with a consistent value between runs
+function mulberry32(seed: number) {
+    return () => {
+        // biome-ignore lint/suspicious/noAssignInExpressions: This is the algorithm
+        let t = (seed += 0x6d2b79f5);
+        t = Math.imul(t ^ (t >>> 15), t | 1);
+        t ^= t + Math.imul(t ^ (t >>> 7), t | 61);
+        return ((t ^ (t >>> 14)) >>> 0) / 4294967296;
+    };
+}
+
+const sizeCache = new Map<string, number>();
+const getSizeForId = (id: string) => {
+    if (sizeCache.get(id) !== undefined) {
+        return sizeCache.get(id);
+    }
+    const size = getRand() * 300 + 40;
+    sizeCache.set(id, size);
+    return size;
+};
+
+const getRand = mulberry32(0);
+
 export default function BidirectionalInfiniteListExample() {
     const [start, setStart] = React.useState(-50);
     const [end, setEnd] = React.useState(50);
     const data = React.useMemo(() => generateItems(end - start + 1, start), [start, end]);
+
     return (
         <LegendList
             data={data}
-            estimatedItemSize={49}
+            estimatedItemSize={30}
             initialScrollIndex={data.length - 1}
             keyExtractor={(it) => it?.id}
             // onEndReached={() => setEnd((e) => e + 50)}
@@ -22,7 +47,14 @@ export default function BidirectionalInfiniteListExample() {
             }}
             onStartReachedThreshold={0.5}
             renderItem={({ item }: { item: SimpleItem }) => (
-                <div style={{ background: "#fff", borderBottom: "1px solid #f0f0f0", padding: 12 }}>
+                <div
+                    style={{
+                        background: "#fff",
+                        borderBottom: "1px solid #ddd",
+                        height: getSizeForId(item.id),
+                        padding: 12,
+                    }}
+                >
                     <div>Item {item.id}</div>
                 </div>
             )}
