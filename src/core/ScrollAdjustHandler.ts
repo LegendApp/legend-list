4c50c629b58ca2b8f36260f848addd54094db5b8
Jay Meistrich <jmeistrich@gmail.com>
2025-10-20 12:19:57 +0200

    refactor commitPendingAdjust

--- src/core/ScrollAdjustHandler.ts
+++ src/core/ScrollAdjustHandler.ts
@@ -2,8 +2,6 @@ import { calculateItemsInView } from "@/core/calculateItemsInView";
 import { Platform } from "@/platform/Platform";
 import { listen$, peek$, type StateContext, set$ } from "@/state/state";
 
-type AdjustKey = "scrollAdjust" | "scrollAdjustPending";
-
 export class ScrollAdjustHandler {
     private appliedAdjust = 0;
     private pendingAdjust = 0;
@@ -12,11 +10,29 @@ export class ScrollAdjustHandler {
 
     constructor(ctx: StateContext) {
         this.context = ctx;
-        listen$(this.context, "scrollingTo", (value) => {
-            if (value === undefined) {
-                this.commitPendingAdjust();
-            }
-        });
+        if (Platform.OS === "web") {
+            // On web, scrollBy during a scrollTo breaks the scrollTo,
+            // so we need to set scrollAdjustPending while doing the scrollTo
+            // and then do the normal scrollBy when it's finished.
+            const commitPendingAdjust = () => {
+                const state = this.context.internalState!;
+                const pending = this.pendingAdjust;
+                this.pendingAdjust = 0;
+
+                this.appliedAdjust += pending;
+                state.scroll += pending;
+                state.scrollForNextCalculateItemsInView = undefined;
+
+                set$(this.context, "scrollAdjustPending", 0);
+                set$(this.context, "scrollAdjust", this.appliedAdjust);
+                calculateItemsInView(this.context, this.context.internalState!);
+            };
+            listen$(this.context, "scrollingTo", (value) => {
+                if (value === undefined) {
+                    commitPendingAdjust();
+                }
+            });
+        }
     }
     requestAdjust(add: number) {
         const scrollingTo = peek$(this.context, "scrollingTo");
@@ -39,14 +55,5 @@ export class ScrollAdjustHandler {
     }
     getAdjust() {
         return this.appliedAdjust;
-    }
-    private commitPendingAdjust() {
-        const pending = this.pendingAdjust;
-        this.pendingAdjust = 0;
-
-        this.appliedAdjust += pending;
-        set$(this.context, "scrollAdjustPending", 0);
-        set$(this.context, "scrollAdjust", this.appliedAdjust);
-        calculateItemsInView(this.context, this.context.internalState!);
     }
 }
