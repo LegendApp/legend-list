ba7b1beb1a88c32eed2aa6fd98e0b3b0c6f4f716
Jay Meistrich <github@jayz.us>
2026-07-07 15:04:48 +0100

    refactor: fold snap offset layout helper

--- src/utils/updateSnapToOffsets.ts
+++ src/utils/updateSnapToOffsets.ts
@@ -1,16 +1,26 @@
 import { getLayoutOffset } from "@/core/layoutAccessors";
-import { getSnapOffsetsForLayout } from "@/core/layoutSnapOffsets";
+import { getContentSize } from "@/state/getContentSize";
 import { type StateContext, set$ } from "@/state/state";
+import { getId } from "@/utils/getId";
+import { toNativeHorizontalOffset } from "@/utils/rtl";
 
 export function updateSnapToOffsets(ctx: StateContext) {
     const state = ctx.state;
     const {
         props: { snapToIndices },
     } = state;
+    const contentSize = state.props.horizontal ? getContentSize(ctx) : undefined;
+    const snapToOffsets: number[] = Array<number>(snapToIndices!.length);
 
-    set$(
-        ctx,
-        "snapToOffsets",
-        getSnapOffsetsForLayout(ctx, snapToIndices!, (index) => getLayoutOffset(ctx, index)),
-    );
+    for (let i = 0; i < snapToIndices!.length; i++) {
+        const index = snapToIndices![i];
+        getId(state, index);
+        const logicalOffset = getLayoutOffset(ctx, index);
+        snapToOffsets[i] =
+            logicalOffset === undefined
+                ? (undefined as unknown as number)
+                : toNativeHorizontalOffset(state, logicalOffset, contentSize);
+    }
+
+    set$(ctx, "snapToOffsets", snapToOffsets);
 }
